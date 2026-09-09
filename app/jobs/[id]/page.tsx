d140d20b41a64f90b101c95032a23e869a579df0
jclagg-del <jclagg@me.com>
2026-09-09 09:40:46 -0400

    Show delivery service type on job details

--- app/jobs/[id]/page.tsx
+++ app/jobs/[id]/page.tsx
@@ -669,7 +669,9 @@ export default function EditJobPage() {
                 <option value="repair">Repair</option>
                 <option value="swap">Swap</option>
                 <option value="roadside">Roadside</option>
-                <option value="delivery">Delivery</option>
+                <option value="Installation">Installation</option>
+                <option value="Delivery">Delivery</option>
+                <option value="Pickup">Pickup</option>
                 <option value="inspection">Inspection</option>
               </select>
             </Field>
